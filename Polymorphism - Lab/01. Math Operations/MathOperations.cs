9a236e271d9eac3a703bf417151a4225568a451e
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 22:12:25 +0200

    Add files via upload

---
+++ Polymorphism - Lab/01. Math Operations/MathOperations.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int one, int two)
+        {
+            return one + two;
+        }
+        public double Add(double one, double two, double three)
+        {
+            return one + two + three;
+        }
+        public decimal Add(decimal one, decimal two, decimal three)
+        {
+            return one + two + three;
+        }
+    }
+}
