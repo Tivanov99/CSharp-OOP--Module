063dbb00c566984a50f9470e56af581a6dec2d39
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 22:20:56 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/05. Create Attribute/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    [Author("rali")]
+  public  class Program
+    {
+        [Author("trix")]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
