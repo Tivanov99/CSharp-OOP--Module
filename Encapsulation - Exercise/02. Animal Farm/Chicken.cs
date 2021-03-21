804cad46e8ae028c6d01b12b9df5f73fe53afd13
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 21:50:44 +0200

    Add files via upload

---
+++ Encapsulation - Exercise/02. Animal Farm/Chicken.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace AnimalFarm.Models
+{
+    public class Chicken
+    {
+        private const int MinAge = 0;
+        private const int MaxAge = 15;
+
+        private string name;
+        private int age;
+
+        public Chicken(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty.");
+                }
+                this.name = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return this.age;
+            }
+
+            protected set
+            {
+                if (value < MinAge || value > MaxAge)
+                {
+                    throw new ArgumentException("Age should be between 0 and 15.");
+                }
+                this.age = value;
+            }
+        }
+
+        public double ProductPerDay
+        {
+            get
+            {
+                return this.CalculateProductPerDay();
+            }
+        }
+
+        private double CalculateProductPerDay()
+        {
+            if (this.Age >= 0 && this.Age <= 3)
+            {
+                return 1.5;
+            }
+            else if (this.Age >= 4 && this.Age <= 7)
+            {
+                return 2;
+            }
+            else if (this.Age >= 8 && this.Age <= 11)
+            {
+                return 1;
+            }
+            return 0.75;
+        }
+    }
+}
