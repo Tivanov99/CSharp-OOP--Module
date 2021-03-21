40a65b418532a625ac0284f8bd478fdeee04f95a
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 22:22:24 +0200

    Add files via upload

---
+++ Reflection and Attributes - Lab/06. Code Tracker/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+
+namespace AuthorProblem
+{
+    [Author("rali")]
+    public class StartUp
+    {
+        [Author("trix")]
+        static void Main(string[] args)
+        {
+            Tracker tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
---
+++ Reflection and Attributes - Lab/06. Code Tracker/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace AuthorProblem
+{
+    public class Tracker
+    {
+        public void PrintMethodsByAuthor()
+        {
+            Type type = typeof(StartUp);
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+            foreach (MethodInfo item in methods)
+            {
+                if (item.CustomAttributes.Any(n => n.AttributeType == typeof(AuthorAttribute)))
+                {
+                    var attributes = item.GetCustomAttributes(false);
+                    foreach (AuthorAttribute att in attributes)
+                    {
+                        Console.WriteLine($"{item.Name} is written by {att.Name}");
+                    }
+                }
+            }
+        }
+    }
+}
