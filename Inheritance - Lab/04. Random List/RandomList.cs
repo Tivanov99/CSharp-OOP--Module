8c1b0ada7aa42c435f112ca852d4f7bc856ead15
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 21:22:31 +0200

    Add files via upload

---
+++ Inheritance - Lab/04. Random List/RandomList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CustomRandomList
+{
+    public class RandomList : List<string>
+    {
+        private Random random;
+        public string RandomString(List<string> input)
+        {
+            random = new Random();
+            int index = random.Next(0, input.Count - 1);
+            string currentElement = input[index];
+            input.RemoveAt(index);
+            return input[index];
+        }
+    }
+}
---
+++ Inheritance - Lab/04. Random List/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList random = new RandomList();
+            Console.WriteLine(random.RandomString(new List<string> { "vladi", "gogo", "rali" }));
+
+        }
+    }
+}
