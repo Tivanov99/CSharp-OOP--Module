4484a8cbfeffe98a379ce9340c9af4ecc8b11ea6
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 22:13:30 +0200

    Add files via upload

---
+++ Polymorphism - Lab/02. Animals/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Animals
+{
+    public class Animal
+    {
+        private string name;
+        private string favouritefood;
+
+        public Animal(string name, string favouritefood)
+        {
+            this.name = name;
+            this.favouritefood = favouritefood;
+        }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {name} and my fovourite food is {favouritefood}";
+        }
+    }
+}
