358b277af016fbeda0ea09cfc0a63bc3e97b8445
Tivanov99 <74056387+Tivanov99@users.noreply.github.com>
2021-03-21 21:56:44 +0200

    Add files via upload

---
+++ Interfaces and Abstraction - Lab/01. Shapes/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Shapes
+{
+   public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int circleinput = int.Parse(Console.ReadLine());
+            int Width = int.Parse(Console.ReadLine());
+            int Length = int.Parse(Console.ReadLine());
+            IDrawable rectangle = new Rectangle(Width, Length);
+            IDrawable circle = new Circle(circleinput);
+            circle.Draw();
+            rectangle.Draw();
+        }
+    }
+}
